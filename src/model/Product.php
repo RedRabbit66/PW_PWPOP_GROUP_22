8f922852783760ad2d9bd59949454e0c784b5c97
llure29 <llorenc.garcia@students.salle.url.edu>
2019-04-06 15:10:44 +0200

    Product Model Modified

--- src/model/Product.php
+++ src/model/Product.php
@@ -27,13 +27,72 @@ class Product
     private $price;
 
     /**
-     * @var string
+     * @var array
      */
-    private $product_image;
+    private $product_image = array();
 
     /**
      * @var int
      */
     private $category;
+
+    /**
+     * Product constructor.
+     * @param string $title
+     * @param string $description
+     * @param int $price
+     * @param array $product_image
+     * @param int $category
+     */
+    public function __construct(string $title, string $description, int $price, array $product_image, int $category)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->price = $price;
+        $this->product_image = $product_image;
+        $this->category = $category;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProductImage(): array
+    {
+        return $this->product_image;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCategory(): int
+    {
+        return $this->category;
+    }
+
+
 
 }
