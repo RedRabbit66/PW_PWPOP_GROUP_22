930c62d0ff534c7f9edd3a98376f2fb64daa279a
llure29 <llorenc.garcia@students.salle.url.edu>
2019-04-07 14:09:32 +0200

    Controller --> Mailer

---
+++ src/Controller/PHPMailer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: llure29
+ * Date: 2019-04-07
+ * Time: 14:04
+ */
+
+namespace SallePW\pwpop\Controller;
+
+
+class PHPMailer implements Mailer
+{
+    private $mail;
+
+    public function __construct(PHPMailer $mail)
+    {
+        $this->mail = $mail;
+    }
+
+    public function sendMail($username, $to, $message) {
+        try {
+            $this->mail->setFrom('pwpop22@outlook.com', 'NO-REPLY');
+            $this->mail->addAddress($to, $username);
+
+            $this->mail->isHTML(true);
+            $this->mail->Subject = 'Pwpop Confirmation email';
+            $this->mail->Body    = $message;
+            $this->mail->AltBody    = $message;
+
+            $this->mail->send();
+        } catch (Exception $e) {
+            echo 'Email has not been sent. Mailer Error: ', $this->mail->ErrorInfo;
+        }
+    }
+}
