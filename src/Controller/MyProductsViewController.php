c23a262dba7959365186fb61e1d08530087dd58f
llure29 <llorenc.garcia@students.salle.url.edu>
2019-05-18 14:57:14 +0200

    MyProducts Funcionant

--- src/Controller/MyProductsViewController.php
+++ src/Controller/MyProductsViewController.php
@@ -47,7 +47,7 @@ class MyProductsViewController
         $service = $this->container->get('get_products_repository');
         $products = $service();
 
-        return $this->container->get('view')->render($response, 'home.html.twig', ['user_id' => $user_id, 'products' => $products]);
+        return $this->container->get('view')->render($response, 'MyProducts.html.twig', ['user_id' => $user_id, 'products' => $products]);
 
 
 
